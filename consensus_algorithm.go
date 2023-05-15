2f0a732fc8029510719c2be6549e6403572153de
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-05-15 14:00:01 +0200

    Add batch transaction capabilities

--- consensus_algorithm.go
+++ consensus_algorithm.go
@@ -1,3 +1,4 @@
 // Add support for fractional token ownership
 // Integrate privacy-preserving mechanisms
 // Fix vulnerabilities in staking contracts
+// Refactor incentive structure in DeFi protocol
