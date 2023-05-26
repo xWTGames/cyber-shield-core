f3256a704d4f968f0fe96712689998e7ac5ad46f
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-05-26 14:00:02 +0200

    Improve contract verification speed

--- transaction_replay_protection.rs
+++ transaction_replay_protection.rs
@@ -2,3 +2,4 @@
 // Improve contract auditing tools
 // Add support for multi-signature wallet contracts
 // Fix errors in contract interaction with wallets
+// Improve smart contract code modularity
