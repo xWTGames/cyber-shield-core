45506e74103b05deda67b73acf8bff80aaea5688
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-05-28 14:00:00 +0200

    Add support for fractional token ownership

--- oracle.sol
+++ oracle.sol
@@ -2,3 +2,4 @@
 // Fix issue with multi-chain transactions
 // Add support for zk-SNARKs in smart contracts
 // Add support for decentralized content distribution
+// Integrate Chainlink oracles for price data
