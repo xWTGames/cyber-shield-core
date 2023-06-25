084679bbb3669d79cba5e20924bb1842b54fa640
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-06-25 14:00:00 +0200

    Bugfix: incorrect reward calculation

--- smart_contract_upgradability.sol
+++ smart_contract_upgradability.sol
@@ -1,3 +1,4 @@
 // Improve decentralized exchange (DEX) liquidity
 // Fix vulnerabilities in staking contracts
 // Refactor storage layout for better efficiency
+// Implement emergency stop mechanism
