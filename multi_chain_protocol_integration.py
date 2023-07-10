6ce22857cd35b5daa5b28b094236d106435166fa
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-07-10 14:00:00 +0200

    Update smart contract tests

--- multi_chain_protocol_integration.py
+++ multi_chain_protocol_integration.py
@@ -1,3 +1,4 @@
 // Add gas optimization features for smart contracts
 // Add decentralized contract upgradeability
 // Integrate zk-rollups for scaling
+// Refactor contract to use EIP-712 signatures
