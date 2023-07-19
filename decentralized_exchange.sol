c637e25d544c7543aee077b8b899ed61700cf78e
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-07-19 14:00:01 +0200

    Refactor multi-signature wallet code

--- decentralized_exchange.sol
+++ decentralized_exchange.sol
@@ -1,3 +1,4 @@
 // Integrate decentralized prediction models
 // Integrate decentralized oracles for data feeds
 // Integrate zk-rollups for scaling
+// Refactor storage layout for better efficiency
