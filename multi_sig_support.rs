a1f9e8be548b83845e8e15ec4f8943b6d6efa1e8
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-08-18 14:00:01 +0200

    Add support for multi-signature wallet contracts

--- multi_sig_support.rs
+++ multi_sig_support.rs
@@ -3,3 +3,4 @@
 // Refactor contract to use EIP-712 signatures
 // Refactor incentive structure in DeFi protocol
 // Fix interoperability issues with other blockchains
+// Refactor multi-asset token management
