f0a50d1b5a91abed7f1fb61379edc3e97a56bf26
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-08-31 14:00:02 +0200

    Improve staking and yield farming logic

--- proof_of_authority.rs
+++ proof_of_authority.rs
@@ -1,3 +1,4 @@
 // Improve security for token minting contracts
 // Implement contract function batching
 // Add native support for staking mechanisms
+// Fix security vulnerability in random number generation
