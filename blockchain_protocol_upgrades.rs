923db99443e94fe5e9fb98716e01758728932720
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-12-02 14:00:01 +0200

    Optimize token minting flow

--- blockchain_protocol_upgrades.rs
+++ blockchain_protocol_upgrades.rs
@@ -1,3 +1,4 @@
 // Enhance decentralized autonomous governance
 // Improve error handling in transaction flow
 // Fix issues with contract state management
+// Add slashing mechanism for malicious actors
