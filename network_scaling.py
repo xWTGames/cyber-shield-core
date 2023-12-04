7eb9937f7b52d1442769cabb5de41687c5ef61d2
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-12-04 14:00:01 +0200

    Optimize WASM compilation for Go contracts

--- network_scaling.py
+++ network_scaling.py
@@ -1,3 +1,4 @@
 // Implement a new contract event system
 // Improve decentralized lending contract flexibility
 // Fix security vulnerability in random number generation
+// Implement a new contract event system
