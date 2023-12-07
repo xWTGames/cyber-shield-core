767a9b8efa785712e81bebe39d46e93b98cfc641
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-12-07 14:00:01 +0200

    Enhance contract event handling

--- proof_of_stake_optimization.py
+++ proof_of_stake_optimization.py
@@ -1,3 +1,4 @@
 // Refactor contract to use EIP-712 signatures
 // Optimize oracle data fetching mechanism
 // Add support for zk-SNARKs in smart contracts
+// Enhance governance contract flexibility
