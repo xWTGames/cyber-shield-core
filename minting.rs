913b32baaf278b69344475304ffeee4f0c51db9e
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-12-09 14:00:01 +0200

    Optimize storage for off-chain data

--- minting.rs
+++ minting.rs
@@ -2,3 +2,4 @@
 // Enhance API rate limiting for smart contracts
 // Refactor decentralized governance structure
 // Enhance contract upgrade mechanism
+// Add support for decentralized credit scoring
