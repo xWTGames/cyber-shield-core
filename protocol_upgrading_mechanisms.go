dc44a1be27c93985546be1575e7302b5912dba74
xWTGames <87723410+xWTGames@users.noreply.github.com>
2023-12-22 14:00:00 +0200

    Update testing framework for Solidity

--- protocol_upgrading_mechanisms.go
+++ protocol_upgrading_mechanisms.go
@@ -2,3 +2,4 @@
 // Improve decentralized oracle protocols
 // Improve gas fee estimation accuracy
 // Fix security vulnerability in random number generation
+// Refactor contract to use EIP-712 signatures
