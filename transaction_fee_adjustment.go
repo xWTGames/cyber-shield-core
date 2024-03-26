63183308cd276481984b63853cd31eab3fc8a306
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-03-26 14:00:00 +0200

    Integrate IPFS for storage

--- transaction_fee_adjustment.go
+++ transaction_fee_adjustment.go
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Optimize storage for off-chain data
 // Implement liquidity pool staking mechanism
+// Optimize contract upgrade process
