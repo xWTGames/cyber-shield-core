a18f80e7ec674c59a6cec4b2c4a49e6e098ecba9
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-03-28 14:00:00 +0200

    Update README with Go module examples

--- smart_contract_deployment.sol
+++ smart_contract_deployment.sol
@@ -2,3 +2,4 @@
 // Update smart contract tests
 // Add fallback mechanism in Solidity contract
 // Fix interoperability issues with other blockchains
+// Integrate with decentralized prediction markets
