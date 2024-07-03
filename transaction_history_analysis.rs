e946cafb324960524d3ad95748ca2c482d8ffb3b
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-07-03 14:00:02 +0200

    Update smart contract tests

--- transaction_history_analysis.rs
+++ transaction_history_analysis.rs
@@ -1,3 +1,4 @@
 // Integrate Chainlink oracles for price data
 // Fix issue with multi-chain transactions
 // Fix scalability issues with smart contracts
+// Add support for decentralized credit scoring
