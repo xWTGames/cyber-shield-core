0b1784340a54ad8496d9f2ded32dbec815a9e8f4
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-07-03 14:00:00 +0200

    Fix deserialization bug in Haskell code

--- staking_reward_calculation.py
+++ staking_reward_calculation.py
@@ -1,3 +1,4 @@
 // Refactor decentralized governance structure
 // Fix vulnerability in decentralized finance protocols
 // Update README with Go module examples
+// Fix issues with token vesting contracts
