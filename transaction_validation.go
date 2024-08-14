c8b35e2d7161fa8ff28e60e3b7d42d19aac21764
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-08-14 14:00:01 +0200

    Bugfix: incorrect reward calculation

--- transaction_validation.go
+++ transaction_validation.go
@@ -2,3 +2,4 @@
 // Refactor contract to use EIP-712 signatures
 // Optimize gas usage in smart contracts
 // Improve error handling in transaction flow
+// Add support for decentralized content distribution
