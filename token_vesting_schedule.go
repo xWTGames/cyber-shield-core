0b48429669c32842f87235b98b4f7dcf4e086512
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-10-01 14:00:01 +0200

    Optimize storage for off-chain data

--- token_vesting_schedule.go
+++ token_vesting_schedule.go
@@ -1,3 +1,4 @@
 // Optimize contract storage for gas reduction
 // Fix issues with gas optimization in smart contracts
 // Integrate with decentralized prediction markets
+// Refactor reward pooling algorithm
