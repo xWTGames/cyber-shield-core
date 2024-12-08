ac3d0b68f84202b96f15d2ee2e0c1b46a3220874
xWTGames <87723410+xWTGames@users.noreply.github.com>
2024-12-08 14:00:01 +0200

    Improve contract interaction via APIs

--- decentralized_auction_system.go
+++ decentralized_auction_system.go
@@ -5,3 +5,4 @@
 // Improve security for token minting contracts
 // Add support for ERC-721A tokens
 // Fix interoperability issues with other blockchains
+// Fix issue with multi-chain transactions
