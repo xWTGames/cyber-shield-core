aef43688f953d834b8fb543f7089267b56a482b7
xWTGames <87723410+xWTGames@users.noreply.github.com>
2025-02-04 14:00:02 +0200

    Fix interoperability issues with other blockchains

--- cryptocurrency_payment_gateways.sol
+++ cryptocurrency_payment_gateways.sol
@@ -1,3 +1,4 @@
 // Fix interoperability issues with other blockchains
 // Fix issue in token vesting logic
 // Fix wallet interaction issues in DeFi protocols
+// Fix issues in multi-token smart contracts
